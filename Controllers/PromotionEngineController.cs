0a76f58269c36eaa634f30b6ea6a7c2ac91af9ab
Aswin Chittaluri <aswinchittaluri@accionlabs.com>
2020-07-26 19:20:52 +0530

    PromotionEngine Source Code

---
+++ Controllers/PromotionEngineController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PromotionEngine.BL;
+using PromotionEngine.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PromotionEngine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromotionEngineController : ControllerBase
+    {
+        public IPromotionBL BL { get; }
+        public PromotionEngineController(IPromotionBL promotionBL)
+        {
+            BL = promotionBL;
+        }
+
+
+        // POST api/<PromotionEnigneController>
+        [HttpPost]
+        public IActionResult Post([FromBody] CartModel cart)
+        {
+            if (ModelState.IsValid && cart?.CartItems.Any() == true)
+            {
+                return Ok(BL.Apply(cart));
+            }
+            return BadRequest();
+        }
+    }
+}
